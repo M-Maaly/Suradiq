b18c55673d2ee99b96ce2e898623cad1c2d05957
M-Maaly <mo7amedm0stafa127@gmail.com>
2026-02-21 06:08:46 +0200

    update

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,4 +1,4 @@
-import FeaturedCarousel from "@/components/features/FeaturedCarousel";
+import {FeaturedCarousel} from "@/components/features/FeaturedCarousel";
 import { sanityFetch } from "@/sanity/lib/live";
 import { ALL_CATEGORIES_QUERY } from "@/sanity/queries/categories";
 import { FEATURED_PRODUCTS_QUERY, FILTER_PRODUCTS_BY_NAME_QUERY, FILTER_PRODUCTS_BY_PRICE_ASC_QUERY, FILTER_PRODUCTS_BY_PRICE_DESC_QUERY, FILTER_PRODUCTS_BY_RELEVANCE_QUERY } from "@/sanity/queries/products";
